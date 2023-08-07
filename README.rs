f97e6c63af69f0a031440a7c16dbec21cc20b47e
Rylan Yancey <rylanyancey@gmail.com>
2023-08-07 14:35:02 -0400

    Added Readme

---
+++ README.rs
@@ -0,0 +1,9 @@
+
+# Canyon Sys
+
+This is a sys crate intended for use with the Canyon Machine Learning Framework. It creates bindings to Cuda and Cudnn using bindgen. 
+
+# Installation 
+
+Canyon-sys looks for the environment variables `CUDNN_ROOT` and `CUDA_ROOT` to function. Export these variables and then build canyon-sys.
+
